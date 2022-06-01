bdcd386a3333f0f9de21222c66aa1b1ee110fd1c
Daylin Morgan <daylinmorgan@gmail.com>
2022-06-01 13:19:19 -0500

    add additional terminal screen padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
         <title>{config.title}</title>
       </Head>
 
-      <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
+      <div className="mx-auto my-20 p-8 h-4/5 w-4/5 overflow-hidden border-2 rounded border-light-yellow dark:border-dark-yellow">
         <div ref={containerRef} className="overflow-y-auto h-full">
           <History history={history} />
 
